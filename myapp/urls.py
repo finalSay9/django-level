f61051b43e3ff2ebebb62d2ad381fa44629793df
finalSay9 <chimwazaevan1@gmail.com>
2026-02-20 15:38:18 +0200

    a change

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HelloView
 
 urlpatterns = [
-    path('hello/', views.hello_world, name='hello_world'),
+    path("hello/", HelloView.as_view(), name="hello_world"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
 
-def hello_world(request):
-    return HttpResponse('hello, world')
+
+class HelloView(APIView):
+    def get(self,request):
+        return Response({
+            'name': 'evan', 'age': 23
+        })
+
+    
