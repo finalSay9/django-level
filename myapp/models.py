64878902e452583fa91468362abaa5c2ad3722a2
finalSay9 <chimwazaevan1@gmail.com>
2026-02-20 20:15:18 +0200

    adding user model

--- myapp/models.py
+++ myapp/models.py
@@ -3,4 +3,11 @@ from django.contrib.auth.model import AbstractBaseUser, BaseUserManager, Permiss
 
 
 class CreateCustomUserManager(BaseUserManager):
-    
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('users must have email')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        
+
