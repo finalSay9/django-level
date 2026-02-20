d0c6132a6514639661484c24b5352a402653f711
finalSay9 <chimwazaevan1@gmail.com>
2026-02-20 20:33:40 +0200

    chnaging some settings

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.model import AbstractBaseUser, BaseUserManager, PermissionMixins
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
-class CreateCustomUserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('users must have email')
@@ -19,10 +19,19 @@ class CreateCustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionMixins):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email =models.EmailField(unique=True)
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)     # ← can access admin?
     date_joined = models.DateTimeField(auto_now_add=True)
+
+
+    USERNAME_FIELD = "email"                          # ← login with email, not username
+    REQUIRED_FIELDS = []                              # fields asked when createsuperuser runs
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
