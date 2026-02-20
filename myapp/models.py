13330959b1a66bf8762ade538fc6d1358014b509
finalSay9 <chimwazaevan1@gmail.com>
2026-02-20 20:23:33 +0200

    creating the customuser model

--- myapp/models.py
+++ myapp/models.py
@@ -9,5 +9,20 @@ class CreateCustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionMixins):
+    email =models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)     # ← can access admin?
+    date_joined = models.DateTimeField(auto_now_add=True)
