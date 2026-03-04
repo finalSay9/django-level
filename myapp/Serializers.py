8a56b9e98c170c6d8712641ab83fbeb5249262c9
finalSay9 <chimwazaevan1@gmail.com>
2026-03-04 17:30:44 +0200

    fixing some errors in serializers.py

--- myapp/Serializers.py
+++ myapp/Serializers.py
@@ -5,8 +5,19 @@ from .models import CustomUser, StudentProfile, TeacherProfile, Subject
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
-        fields = ['id', 'name']
-    
+        fields = ['id', 'name', 'short_code','department']
 
 
+class MinimalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'full_name', 'primary_role', 'gender']
+       
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+    user = MinimalUserSerializer(read_only=True)
+    fields = [
+            'id', 'user', 'form_level', 'stream',
+            'guardian_name', 'guardian_phone', 'profile_picture'
+        ]
 
